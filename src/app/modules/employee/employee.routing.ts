f40abd408b979d1f414c759244a8690c8d646747
Ashok Bhattarai <eashokbhattarai@gmail.com>
2021-02-25 17:16:38 +0545

    Employee Form Management Frontend

---
+++ src/app/modules/employee/employee.routing.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../../services/_guards/auth-guard';
+import { EmployeeComponent } from './components/employee.component';
+const routes: Routes = [
+    {
+      path: '',
+      children: [
+        {path: '', component:EmployeeComponent}
+      ]
+    }
+  ];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class EmployeeRoutingModule { }
